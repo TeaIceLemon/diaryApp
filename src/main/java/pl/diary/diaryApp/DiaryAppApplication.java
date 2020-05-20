fb3771e19bded1cc62134fb6a8f07a3f9bfc6fa5
TeaIceLemon <piotr.wieczorek1993@gmail.com>
2020-05-20 21:14:36 +0200

    First commit

---
+++ src/main/java/pl/diary/diaryApp/DiaryAppApplication.java
@@ -0,0 +1,13 @@
+package pl.diary.diaryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryAppApplication.class, args);
+	}
+
+}
